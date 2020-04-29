09645f036fcde1a76b71c34daba9bbf9ce6756fc
Raphaaal <azorin.raphael@gmail.com>
2020-04-29 14:05:25 +0200

    Working connection to kafka cluster

---
+++ src/main/scala/org/myorg/quickstart/FraudDetector.scala
@@ -0,0 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.myorg.quickstart
+
+import java.util.Properties
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+
+object FraudDetector {
+  def main(args: Array[String]) {
+    // set up the streaming execution environment
+    val env = StreamExecutionEnvironment.getExecutionEnvironment
+
+    val properties = new Properties()
+    properties.setProperty("bootstrap.servers", "localhost:9092")
+    // only required for Kafka 0.8
+    properties.setProperty("zookeeper.connect", "localhost:2181")
+    properties.setProperty("group.id", "test")
+    val stream = env
+      .addSource(new FlinkKafkaConsumer[String]("clicks", new SimpleStringSchema(), properties))
+
+    // TODO : deserialiser en JSON plutot qu'en string pour pouvoir faire :
+    //  ex : stream.keyBy(...).map(elem => elem)
+
+      .print()
+
+    // execute program
+    env.execute("Test Job")
+  }
+}
