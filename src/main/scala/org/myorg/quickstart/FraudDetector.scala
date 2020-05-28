4a11f7531557ed2476e5c3360d042db6aaa57fdb
Raphaaal <azorin.raphael@gmail.com>
2020-05-28 19:16:05 +0200

    Add event time windowing

--- src/main/scala/org/myorg/quickstart/FraudDetector.scala
+++ src/main/scala/org/myorg/quickstart/FraudDetector.scala
@@ -17,41 +17,70 @@
  */
 
 package org.myorg.quickstart
-
 import java.util.Properties
 
-import org.apache.flink.api.common.serialization.SimpleStringSchema
-import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
+import org.apache.flink.streaming.util.serialization.{JSONKeyValueDeserializationSchema, SimpleStringSchema}
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer
+import org.apache.flink.api.common.functions.{AggregateFunction, MapFunction}
+import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.JsonNode
+import org.apache.flink.streaming.api.TimeCharacteristic
+import org.apache.flink.streaming.api.scala.DataStream
+import org.apache.flink.streaming.api.windowing.assigners.TumblingEventTimeWindows
+import org.apache.flink.streaming.api.windowing.time.Time
 
-import scala.util.parsing.json.JSONObject
 
 object FraudDetector {
   def main(args: Array[String]) {
-    // set up the streaming execution environment
+
+    // Set up the streaming execution environment
+
     val env = StreamExecutionEnvironment.getExecutionEnvironment
+    env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
 
     val properties = new Properties()
     properties.setProperty("bootstrap.servers", "localhost:9092")
-    // only required for Kafka 0.8
-    properties.setProperty("zookeeper.connect", "localhost:2181")
+    properties.setProperty("zookeeper.connect", "localhost:2181") // only required for Kafka 0.8
     properties.setProperty("group.id", "test")
 
-    val kafkaConsumer = new FlinkKafkaConsumer(
+    val kafkaConsumer = new FlinkKafkaConsumer[ObjectNode](
       "clicks",
       new JSONKeyValueDeserializationSchema(false),
       properties
     )
-    //val stream = env.addSource(new FlinkKafkaConsumer[String]("clicks", new SimpleStringSchema(), properties))
-    val stream = env.addSource(kafkaConsumer)
 
-      // TODO : deserialiser en JSON plutot qu'en string pour pouvoir faire :
-      //  ex : stream.keyBy(...).map(elem => elem)
+    // Set up the event stream
 
-      .print()
+    // val stream = env.addSource(new FlinkKafkaConsumer[String]("clicks", new SimpleStringSchema(), properties))
+    val stream : DataStream[ObjectNode] = env.addSource(kafkaConsumer).name("clicks")
+    val streamValue : DataStream[JsonNode] = stream.map(value => value.get("value"))
+    val streamWithTimestampsAndWatermarks = streamValue.assignAscendingTimestamps( _.get("timestamp").asLong())
 
-    // execute program
+    streamWithTimestampsAndWatermarks.print()
+
+    // Aggregate on stream keyed by IP
+
+    /**
+     * The accumulator is used to keep a running sum and a count. The [getResult] method
+     * computes the average.
+     */
+    class AverageAggregate extends AggregateFunction[JsonNode, Long, Double] {
+      override def createAccumulator() = 0
+
+      override def add(value: JsonNode, accumulator: Long) = accumulator + 1
+
+      override def getResult(accumulator: Long) = accumulator
+
+      override def merge(a: Long, b: Long) =  a + b
+    }
+
+    val count = streamWithTimestampsAndWatermarks
+      .keyBy(_.get("ip"))
+      .window(TumblingEventTimeWindows.of(Time.seconds(5)))
+      .aggregate(new AverageAggregate)
+
+    // Execute program
     env.execute("Fraud detection")
   }
 }
