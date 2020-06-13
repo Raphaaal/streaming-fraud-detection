4e8d66d0873e27d4adf00d2460640476516391d8
Raphaaal <azorin.raphael@gmail.com>
2020-06-13 12:47:59 +0200

    Implement 3 patterns outputing fraudulent IPs

--- src/main/scala/org/myorg/quickstart/FraudDetector.scala
+++ src/main/scala/org/myorg/quickstart/FraudDetector.scala
@@ -19,6 +19,7 @@
 package org.myorg.quickstart
 import java.util.Properties
 
+import org.apache.flink.api.common.functions.CoGroupFunction
 import org.apache.flink.configuration.{ConfigConstants, Configuration, RestOptions}
 import org.apache.flink.shaded.jackson2.com.fasterxml.jackson.databind.node.ObjectNode
 import org.apache.flink.streaming.util.serialization.JSONKeyValueDeserializationSchema
@@ -34,6 +35,7 @@ import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.api.watermark.Watermark
 import org.apache.flink.streaming.api.windowing.triggers.{EventTimeTrigger, PurgingTrigger}
 import org.apache.flink.streaming.api.windowing.windows.{TimeWindow, Window}
+import org.apache.flink.util.Collector
 
 import scala.math.max
 
@@ -44,53 +46,107 @@ object FraudDetector {
     // Set up the streaming execution environment
     val config = new Configuration()
     config.setInteger(RestOptions.PORT, 8082)
-    val env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(config)
-    //val env = StreamExecutionEnvironment.getExecutionEnvironment
+    val env = StreamExecutionEnvironment.createLocalEnvironmentWithWebUI(config) // val env = StreamExecutionEnvironment.getExecutionEnvironment
     env.setStreamTimeCharacteristic(TimeCharacteristic.EventTime)
-    env.setParallelism(1) // test
 
+    // Set up sources properties
     val properties = new Properties()
     properties.setProperty("bootstrap.servers", "localhost:9092")
-    properties.setProperty("zookeeper.connect", "localhost:2181") // only required for Kafka 0.8
+    properties.setProperty("zookeeper.connect", "localhost:2181")
     properties.setProperty("group.id", "test")
 
-    val kafkaConsumer = new FlinkKafkaConsumer[ObjectNode](
+    // Set up Kafka consumers sources
+    val kafkaConsumer_clicks = new FlinkKafkaConsumer[ObjectNode](
       "clicks",
       new JSONKeyValueDeserializationSchema(false),
       properties
     )
+    val kafkaConsumer_displays = new FlinkKafkaConsumer[ObjectNode](
+      "displays",
+      new JSONKeyValueDeserializationSchema(false),
+      properties
+    )
 
-    // Set up the event stream
+    // Set up the event streams
+    val stream_clicks : DataStream[ObjectNode] = env.addSource(kafkaConsumer_clicks).name("clicks")
+    val streamValue_clicks : DataStream[JsonNode] = stream_clicks.map {value => value.get("value") }
+    val stream_displays : DataStream[ObjectNode] = env.addSource(kafkaConsumer_displays).name("displays")
+    val streamValue_displays : DataStream[JsonNode] = stream_displays.map {value => value.get("value") }
 
-    val stream : DataStream[ObjectNode] = env.addSource(kafkaConsumer).name("clicks")
-    val streamValue : DataStream[JsonNode] = stream.map {value => value.get("value") }
-
-    // Set up event time with watermarks
-
+    // Set up timestamps and watermarks
     class PunctuatedAssigner extends AssignerWithPunctuatedWatermarks[JsonNode] {
-
       override def extractTimestamp(element: JsonNode, previousElementTimestamp: Long): Long = {
         element.get("timestamp").asLong()*1000
       }
-
       override def checkAndGetNextWatermark(lastElement: JsonNode, extractedTimestamp: Long): Watermark = {
         new Watermark(extractedTimestamp)
       }
     }
+    val streamEventTime_clicks : DataStream[JsonNode] = streamValue_clicks.assignTimestampsAndWatermarks(new PunctuatedAssigner)
+    val streamEventTime_displays : DataStream[JsonNode] = streamValue_displays.assignTimestampsAndWatermarks(new PunctuatedAssigner)
 
-    val streamEventTime : DataStream[JsonNode] = streamValue.assignTimestampsAndWatermarks(new PunctuatedAssigner)
+    // Format input events
+    val ipStream_clicks : DataStream[(String, Int)] = streamEventTime_clicks.map(value => (value.get("ip").asText(), 1))
+    val ipStream_displays : DataStream[(String, Int)] = streamEventTime_displays.map(value => (value.get("ip").asText(), 1))
+    val WindowTimeSecs = 600
 
-    // Aggregate on stream keyed by IP
-
-    val ipStream : DataStream[(String, Int)] = streamEventTime.map(value => (value.get("ip").asText(), 1))
-    val clicks_count : DataStream[(String, Int)] = ipStream
+    // Aggregate on streams keyed by IP
+    val clicks_count : DataStream[(String, Int)] = ipStream_clicks
       .keyBy(0)
-      .window(TumblingEventTimeWindows.of(Time.minutes(60))) // .window(TumblingProcessingTimeWindows.of(Time.seconds(20)))
+      .window(TumblingEventTimeWindows.of(Time.seconds(WindowTimeSecs))) // .window(TumblingProcessingTimeWindows.of(Time.seconds(20)))
       .reduce { (v1, v2) => (v1._1, v1._2 + v2._2) }
-      .filter {value => value._2 >= 6}
+    val displays_count : DataStream[(String, Int)] = ipStream_displays
+      .keyBy(0)
+      .window(TumblingEventTimeWindows.of(Time.seconds(WindowTimeSecs))) // .window(TumblingProcessingTimeWindows.of(Time.seconds(20)))
+      .reduce { (v1, v2) => (v1._1, v1._2 + v2._2) }
 
-    // Should output to proper Flink Sink (with checkpointing)
-    clicks_count.rebalance.writeAsText("output/clicks_ip_output.txt").setParallelism(1)
+    // PATTERN 1: nb clicks / IP
+    // TODO: Should output the full fraudulent event (not just the IP)
+    val clicks_count_fraud : DataStream[(String, Int)] = clicks_count.filter {value => value._2 >= 6}
+    clicks_count_fraud
+      .rebalance.writeAsText("output/clicks_ip_output.txt", org.apache.flink.core.fs.FileSystem.WriteMode.OVERWRITE)
+      .setParallelism(1) // Should output to proper Flink Sink (with checkpointing)
+
+    // PATTERN 2: CTR / IP
+    //  TODO: should be by UID and with specific thresolds
+    val streamJoined : DataStream[(String, Double)] = displays_count.join(clicks_count)
+      .where(value => value._1).equalTo(value => value._1)
+      .window(TumblingEventTimeWindows.of(Time.seconds(WindowTimeSecs)))
+      .apply { (e1, e2) => (e1._1, 1.0 * (e1._2 / e2._2)) }
+    val ctr_fraud = streamJoined.filter {value => value._2 >= 0.25}
+    ctr_fraud.print
+
+    // PATTERN 3: nb displays / IP
+    // TODO: Should output the full fraudulent event (not just the IP)
+    val displays_count_fraud : DataStream[(String, Int)] = displays_count.filter {value => value._2 >= 6}
+    displays_count_fraud
+      .rebalance.writeAsText("output/displays_ip_output.txt", org.apache.flink.core.fs.FileSystem.WriteMode.OVERWRITE)
+      .setParallelism(1) // Should output to proper Flink Sink (with checkpointing)
+  /*
+    // Aggregate on two joined streams
+    class CTR extends CoGroupFunction[(String, Int), (String, Int), (String, Int, Int)] {
+
+      override def coGroup(
+                            leftElements : java.lang.Iterable[(String, Int)],
+                            rightElements: java.lang.Iterable[(String, Int)],
+                            out: Collector[(String, Int, Int)]
+                          ): (String, Int, Int) = {
+        val NullElement = -1
+
+        for (leftElem <- leftElements) {
+          var hadElements = false
+          for (rightElem <- rightElements) {
+            out.collect( (leftElem._0 , rightElem._1, rightElem._1) : (String, Int, Int) )
+            hadElements = true
+          }
+          if (!hadElements) {
+            out.collect( (leftElem._0 , None))
+          }
+        }
+      }
+    }
+
+   */
 
     // Execute program
     env.execute("Fraud detection")
